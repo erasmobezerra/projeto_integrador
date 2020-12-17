a294caa4ce86c115442ed7d7afbfd49bd74529ae
Erasmo Ribeiro Bezerra <erasmo.ads.tech@gmail.com>
2020-12-17 17:21:55 -0300

    implementando menu responsivo

---
+++ menu_responsivo/menu.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu responsivo</title>
+    <link rel="stylesheet" href="style_menu.css"> 
+
+</head>
+<body>
+
+<input type="checkbox" id="bt_menu">
+<label for="bt_menu">&#9776;</label>
+
+    <nav class="menu">
+        <ul>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Servicos</a>
+                    <ul>
+                        <li><a href="#">Criação</a></li>
+                        <li><a href="#">Arte Visual</a></li>
+                    </ul>
+            </li>
+            <li>
+                <a href="#">Cursos</a>
+                    <ul>
+                        <li><a href="#">Javas</a></li>
+                        <li><a href="#">Photoshop</a></li>
+                        <li><a href="#">HTML/CSS</a></li>
+                    </ul>
+            </li>
+            <li><a href="#">Contato</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
