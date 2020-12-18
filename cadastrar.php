1ec9aced9df25ddc06b1c18561382a1534f3d02e
Erasmo Ribeiro Bezerra <erasmo.ads.tech@gmail.com>
2020-12-18 01:09:16 -0300

    reorganizando arquivos

---
+++ cadastrar.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+include("conexao.php");
+
+$nome = filter_input(INPUT_POST,'nome');
+$telefone = filter_input(INPUT_POST,'telefone');
+$email = filter_input(INPUT_POST,'email');
+$senha = filter_input(INPUT_POST,'senha');
+
+	if ($nome && $telefone && $email && $senha) {
+		
+		$sql = "SELECT count(*) AS total FROM usuarios WHERE email = '$email'";
+		$result = mysqli_query($conexao, $sql);
+		$row = mysqli_fetch_assoc($result);
+
+		if($row['total'] > 0) {
+			$_SESSION['usuario_existe'] = "O usuário escolhido já existe";
+			header('Location: cadastro.php');
+			exit;
+
+		} else {
+
+			$sql = "INSERT INTO usuarios (nome, telefone, email, senha) VALUES ('$nome', '$telefone', '$email', md5('$senha'))";
+			$resultado_usuario = mysqli_query($conexao, $sql);
+	
+			if(mysqli_insert_id($conexao)){
+				$_SESSION['msg'] = "Usuário cadastrado com sucesso";
+				header("Location: cadastro.php");
+	
+			}else{
+				$_SESSION['msg'] = "Usuário não foi cadastrado com sucesso";
+				header("Location: cadastro.php");
+			}
+	
+			$conexao->close();
+			header('Location: cadastro.php');
+			exit;
+	
+		} 
+
+	} else {
+		$_SESSION['campo_em_aberto'] = "Há campo(s) em aberto! Tente novamente.</p>";
+		header("Location: cadastro.php");
+		exit;
+	}			
+
+?>
---
+++ home.php
@@ -0,0 +1,52 @@
+<?php
+include('verifica_login.php');
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="./css/style_menu.css">
+    <link rel="stylesheet" href="./css/style_corpo.css">
+</head>
+<body>
+    <input type="checkbox" id="bt_menu">
+    <label for="bt_menu">&#9776;</label>
+
+    <nav class="menu">
+        <ul>
+            <li><a href="home.php">Home</a></li>
+            <li>
+                <a href="#">Aplicativos</a>
+                    <ul>
+                        <li><a href="app_desktop.php">App Desktop</a></li>
+                        <li><a href="app_mobile.php">App Mobile</a></li>
+                    </ul>
+            </li>
+            <li><a href="rede_local.php">Rede Local</a></li>
+            <li><a href="requisitos.php">Requisitos</a></li>
+            <li><a href="avaliacao.php">Avaliação</a></li>
+            <li><a href="contato.php">Contato</a></li>
+            <li>
+                <a href="#">Logout</a>
+                    <ul>
+                        <li><a href="logout.php">Clique aqui para sair</a></li>
+                    </ul>         
+            </li>
+            <li class="boasvindas">Olá, <?php echo $_SESSION['nome'];?></li>
+        </ul>
+        
+    </nav>
+    
+    <div class="container">
+        
+        <h1>Início do texto</h1>
+       
+    </div>
+    
+</body>
+</html>
+ 
